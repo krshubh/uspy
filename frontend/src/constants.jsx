b7fa8a154fe6417eb77b1877f255878de42e65a9
Shubham Kumar <shubham.kumar01@quantiphi.com>
2023-05-15 13:27:31 +0530

    changed constants

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,57 +1,49 @@
 // export const HOST_NAME = "192.168.1.78";
 // export const HOST_NAME = "localhost";
-export const HOST_NAME = "34.131.95.131";
+export const HOST_NAME = "api.uspy.in";
 // export const HOST_NAME = "34.126.219.211";
-export const PORT = "8000";
+// export const PORT = "8000";
 // Api
-export const TOKEN_API = "http://" + HOST_NAME + ":" + PORT + "/api/token/";
-export const REFRESH_TOKEN_API =
-  "http://" + HOST_NAME + ":" + PORT + "/api/token/refresh/";
-export const SIGN_UP_API = "http://" + HOST_NAME + ":" + PORT + "/api/signup/";
-export const LOGIN_API = "http://" + HOST_NAME + ":" + PORT + "/api/login/";
-export const PROFILE_API = "http://" + HOST_NAME + ":" + PORT + "/api/profile/";
+export const TOKEN_API = "http://" + HOST_NAME + "/token/";
+export const REFRESH_TOKEN_API = "http://" + HOST_NAME + "/token/refresh/";
+export const SIGN_UP_API = "http://" + HOST_NAME + "/signup/";
+export const LOGIN_API = "http://" + HOST_NAME + "/login/";
+export const PROFILE_API = "http://" + HOST_NAME + "/profile/";
 
-export const CHANGE_PASSWORD_API =
-  "http://" + HOST_NAME + ":" + PORT + "/api/change_password/";
-export const USER_SEARCH =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user?search=";
+export const CHANGE_PASSWORD_API = "http://" + HOST_NAME + "/change_password/";
+export const USER_SEARCH = "http://" + HOST_NAME + "/user?search=";
 
 // PARENT
-export const GET_PARENTS =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/parent/";
+export const GET_PARENTS = "http://" + HOST_NAME + "/user/parent/";
 export const ADD_PARENTS_REQUEST =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/parent/add_request/";
+  "http://" + HOST_NAME + "/user/parent/add_request/";
 export const REMOVE_REQUESTED_PARENT =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/parent/delete_requested/";
+  "http://" + HOST_NAME + "/user/parent/delete_requested/";
 export const REMOVE_PARENT_REQUEST =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/parent/delete_request/";
+  "http://" + HOST_NAME + "/user/parent/delete_request/";
 export const REMOVE_CONFIRMED_PARENT =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/parent/delete_confirmed/";
+  "http://" + HOST_NAME + "/user/parent/delete_confirmed/";
 export const ACCEPT_PARENT_REQUEST =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/parent/confirm_request/";
+  "http://" + HOST_NAME + "/user/parent/confirm_request/";
 
 // CHILDREN
-export const GET_CHILDREN =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/children/";
+export const GET_CHILDREN = "http://" + HOST_NAME + "/user/children/";
 export const ADD_CHILDREN_REQUEST =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/children/add_request/";
+  "http://" + HOST_NAME + "/user/children/add_request/";
 export const REMOVE_REQUESTED_CHILDREN =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/children/delete_requested/";
+  "http://" + HOST_NAME + "/user/children/delete_requested/";
 export const REMOVE_CHILDREN_REQUEST =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/children/delete_request/";
+  "http://" + HOST_NAME + "/user/children/delete_request/";
 export const REMOVE_CONFIRMED_CHILDREN =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/children/delete_confirmed/";
+  "http://" + HOST_NAME + "/user/children/delete_confirmed/";
 export const ACCEPT_CHILDREN_REQUEST =
-  "http://" + HOST_NAME + ":" + PORT + "/api/user/children/confirm_request/";
+  "http://" + HOST_NAME + "/user/children/confirm_request/";
 
-export const CALL_LOG_API =
-  "http://" + HOST_NAME + ":" + PORT + "/api/call_log/";
-export const CALL_LOG_CHILD_API =
-  "http://" + HOST_NAME + ":" + PORT + "/api/call_log/children/";
+export const CALL_LOG_API = "http://" + HOST_NAME + "/call_log/";
+export const CALL_LOG_CHILD_API = "http://" + HOST_NAME + "/call_log/children/";
 
-export const MESSAGE_API = "http://" + HOST_NAME + ":" + PORT + "/api/message/";
-export const MESSAGE_CHILD_API =
-  "http://" + HOST_NAME + ":" + PORT + "/api/message/children/";
+export const MESSAGE_API = "http://" + HOST_NAME + "/message/";
+export const MESSAGE_CHILD_API = "http://" + HOST_NAME + "/message/children/";
 
 // Constants
 export const PARENT_REQUESTS = "requests";
