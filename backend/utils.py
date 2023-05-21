702dec4d6b4152f70970c61353f85a1f5c7d39d3
Shubham Kumar <shubham.kumar01@quantiphi.com>
2023-05-21 20:25:57 +0530

    some changes in urls

--- backend/utils.py
+++ backend/utils.py
@@ -3,10 +3,13 @@ from django.core.mail import send_mail
 
 def send_message(subject,
                  message):
-    send_mail(
-        subject,
-        message,
-        'admin@uspy.in',
-        ['support@uspy.in'],
-        fail_silently=False,
-    )
+    try:
+        send_mail(
+            subject,
+            message,
+            'admin@uspy.in',
+            ['support@uspy.in'],
+            fail_silently=False,
+        )
+    except e:
+        print(e)
